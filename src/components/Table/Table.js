ea969a3f1420064b6adff1c8c657385f2ac64fbd
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-01-26 16:52:01 +0200

    MVP done. TODO: some improvements and styles

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,37 +1,111 @@
+import { useState, useEffect, useMemo } from 'react';
+
 export default function Table({ applicants, deleteContact }) {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <th>firstName</th>
-          <th>lastName</th>
-          <th>phone</th>
-          <th>gender</th>
-          <th>age</th>
-          <th>DELETE</th>
-        </tr>
-        {applicants.map(item => {
-          return (
-            <tr key={item.id}>
-              <td>{item.firstName}</td>
-              <td>{item.lastName}</td>
-              <td>{item.phone}</td>
-              <td>{item.gender}</td>
-              <td>{item.age}</td>
-              <td>
-                <button
-                  className="button"
-                  type="button"
-                  id={item.id}
-                  onClick={deleteContact}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+    const [sortConfig, setSortConfig] = useState({
+        sortField: null,
+        direction: null,
+    });
+
+    let sortedApplicants = [...applicants];
+
+    if (sortConfig.sortField !== null) {
+        sortedApplicants.sort((a, b) => {
+            if (a[sortConfig.sortField] < b[sortConfig.sortField]) {
+                return sortConfig.direction === 'ascending' ? -1 : 1;
+            }
+            if (a[sortConfig.sortField] > b[sortConfig.sortField]) {
+                return sortConfig.direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
+    }
+
+    const requestSort = sortField => {
+        let direction = 'ascending';
+        if (
+            sortConfig.sortField === sortField &&
+            sortConfig.direction === 'ascending'
+        ) {
+            direction = 'descending';
+        }
+        setSortConfig({ sortField, direction });
+    };
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <button
+                            onClick={() => {
+                                requestSort('firstName');
+                            }}
+                        >
+                            firstName
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            onClick={() => {
+                                requestSort('lastName');
+                            }}
+                        >
+                            lastName
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            onClick={() => {
+                                requestSort('phone');
+                            }}
+                        >
+                            phone
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            onClick={() => {
+                                requestSort('gender');
+                            }}
+                        >
+                            gender
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            onClick={() => {
+                                requestSort('age');
+                            }}
+                        >
+                            age
+                        </button>
+                    </th>
+                    <th>DELETE</th>
+                </tr>
+            </thead>
+            <tbody>
+                {sortedApplicants.map(item => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.firstName}</td>
+                            <td>{item.lastName}</td>
+                            <td>{item.phone}</td>
+                            <td>{item.gender}</td>
+                            <td>{item.age}</td>
+                            <td>
+                                <button
+                                    className="button"
+                                    type="button"
+                                    id={item.id}
+                                    onClick={deleteContact}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
