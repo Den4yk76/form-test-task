52598558948f32f37c8d65bfd48aed47b2dac114
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-01-27 16:57:35 +0200

    style and functional improvements

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,126 +2,130 @@ import { useState, useEffect } from 'react';
 import './index.css';
 
 export default function Form({ addContact }) {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [gender, setGender] = useState('');
-  const [age, setAge] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState('');
 
-  const resetFields = () => {
-    setFirstName('');
-    setLastName('');
-    setPhone('');
-    setGender('');
-    setAge('');
-    document.getElementById('gender_male').checked = false;
-    document.getElementById('gender_female').checked = false;
-  };
+    const resetFields = () => {
+        setFirstName('');
+        setLastName('');
+        setPhone('');
+        setGender('');
+        setAge('');
+        document.getElementById('gender_male').checked = false;
+        document.getElementById('gender_female').checked = false;
+    };
 
-  const onSubmit = e => {
-    e.preventDefault();
-    addContact(firstName, lastName, phone, gender, age);
-    resetFields();
-  };
+    const onSubmit = e => {
+        e.preventDefault();
+        addContact(firstName, lastName, phone, gender, age);
+        resetFields();
+    };
 
-  return (
-    <form onSubmit={onSubmit}>
-      <label className="label" for="first-name">
-        First Name:
-      </label>
-      <input
-        className="form-input"
-        type="text"
-        id="first-name"
-        name="first-name"
-        value={firstName}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="The name can only contain letters, an apostrophe, a dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan, etc."
-        required
-        onChange={e => {
-          setFirstName(e.target.value);
-        }}
-      />
-      <label className="label" for="last-name">
-        Last name:
-      </label>
-      <input
-        className="form-input"
-        type="text"
-        id="last-name"
-        name="last-name"
-        value={lastName}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="The name can only contain letters, an apostrophe, a dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan, etc."
-        required
-        onChange={e => {
-          setLastName(e.target.value);
-        }}
-      />
-      <label className="label" for="phone">
-        Phone:
-      </label>
-      <input
-        className="form-input"
-        type="tel"
-        name="phone"
-        id="phone"
-        value={phone}
-        required
-        onChange={e => {
-          setPhone(e.target.value);
-        }}
-      />
-      <label className="label" for="label-group">
-        Gender:
-        <div id="label-group">
-          <input
-            className="gender-input"
-            required
-            type="radio"
-            name="gender"
-            id="gender_male"
-            value="1"
-            onChange={e => {
-              setGender(e.target.value);
-            }}
-          />
-          <label className="gender-label" for="gender">
-            Male
-          </label>
-          <input
-            className="gender-input"
-            required
-            type="radio"
-            name="gender"
-            id="gender_female"
-            value="0"
-            onChange={e => {
-              setGender(e.target.value);
-            }}
-          />
-          <label className="gender-label" for="gender">
-            Female
-          </label>
-        </div>
-      </label>
-      <label className="label" for="age">
-        Age:
-      </label>
-      <input
-        className="form-input"
-        type="text"
-        id="age"
-        name="age"
-        value={age}
-        required
-        onChange={e => {
-          setAge(e.target.value);
-        }}
-      />
-      <button className="form-button" type="submit" value="Submit">
-        Add friend
-      </button>
-    </form>
-  );
+    return (
+        <form onSubmit={onSubmit}>
+            <label className="label" htmlFor="first-name">
+                First Name:
+            </label>
+            <input
+                className="form-input form-input--validation"
+                type="text"
+                id="first-name"
+                name="first-name"
+                value={firstName}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                placeholder="Type your first name"
+                title="The name can only contain letters, an apostrophe, a dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan, etc."
+                required
+                onChange={e => {
+                    setFirstName(e.target.value);
+                }}
+            />
+            <label className="label" htmlFor="last-name">
+                Last name:
+            </label>
+            <input
+                className="form-input form-input--validation"
+                type="text"
+                id="last-name"
+                name="last-name"
+                value={lastName}
+                placeholder="Type your last name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="The name can only contain letters, an apostrophe, a dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan, etc."
+                required
+                onChange={e => {
+                    setLastName(e.target.value);
+                }}
+            />
+            <label className="label" htmlFor="phone">
+                Phone:
+            </label>
+            <input
+                className="form-input form-input--validation"
+                type="tel"
+                name="phone"
+                placeholder="Type your phone"
+                id="phone"
+                value={phone}
+                required
+                onChange={e => {
+                    setPhone(e.target.value);
+                }}
+            />
+            <label className="label" htmlFor="label-group">
+                Gender:
+                <div id="label-group">
+                    <input
+                        className="gender-input"
+                        required
+                        type="radio"
+                        name="gender"
+                        id="gender_male"
+                        value="1"
+                        onChange={e => {
+                            setGender(e.target.value);
+                        }}
+                    />
+                    <label className="gender-label" htmlFor="gender">
+                        Male
+                    </label>
+                    <input
+                        className="gender-input"
+                        required
+                        type="radio"
+                        name="gender"
+                        id="gender_female"
+                        value="0"
+                        onChange={e => {
+                            setGender(e.target.value);
+                        }}
+                    />
+                    <label className="gender-label" htmlFor="gender">
+                        Female
+                    </label>
+                </div>
+            </label>
+            <label className="label" htmlFor="age">
+                Age:
+            </label>
+            <input
+                className="form-input form-input--validation"
+                type="text"
+                id="age"
+                name="age"
+                placeholder="Type your age"
+                value={age}
+                required
+                onChange={e => {
+                    setAge(e.target.value);
+                }}
+            />
+            <button className="form-button" type="submit" value="Submit">
+                Add friend
+            </button>
+        </form>
+    );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,111 +2,134 @@ import { useState, useEffect, useMemo } from 'react';
 import './index.css';
 
 export default function Table({ applicants, deleteContact }) {
-  const [sortConfig, setSortConfig] = useState({
-    sortField: null,
-    direction: null,
-  });
-
-  let sortedApplicants = [...applicants];
-
-  if (sortConfig.sortField !== null) {
-    sortedApplicants.sort((a, b) => {
-      if (a[sortConfig.sortField] < b[sortConfig.sortField]) {
-        return sortConfig.direction === 'ascending' ? -1 : 1;
-      }
-      if (a[sortConfig.sortField] > b[sortConfig.sortField]) {
-        return sortConfig.direction === 'ascending' ? 1 : -1;
-      }
-      return 0;
+    const [sortConfig, setSortConfig] = useState({
+        sortField: null,
+        direction: null,
     });
-  }
 
-  const requestSort = sortField => {
-    let direction = 'ascending';
-    if (
-      sortConfig.sortField === sortField &&
-      sortConfig.direction === 'ascending'
-    ) {
-      direction = 'descending';
+    let sortedApplicants = [...applicants];
+
+    if (sortConfig.sortField !== null) {
+        sortedApplicants.sort((a, b) => {
+            if (a[sortConfig.sortField] < b[sortConfig.sortField]) {
+                return sortConfig.direction === 'ascending' ? -1 : 1;
+            }
+            if (a[sortConfig.sortField] > b[sortConfig.sortField]) {
+                return sortConfig.direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
     }
-    setSortConfig({ sortField, direction });
-  };
 
-  return (
-    <table className="table">
-      <thead className="table-head">
-        <tr className="table-head">
-          <th className="table-head">
-            <button
-              onClick={() => {
-                requestSort('firstName');
-              }}
-            >
-              firstName
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => {
-                requestSort('lastName');
-              }}
-            >
-              lastName
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => {
-                requestSort('phone');
-              }}
-            >
-              phone
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => {
-                requestSort('gender');
-              }}
-            >
-              gender
-            </button>
-          </th>
-          <th>
-            <button
-              onClick={() => {
-                requestSort('age');
-              }}
-            >
-              age
-            </button>
-          </th>
-          <th>DELETE</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedApplicants.map(item => {
-          return (
-            <tr key={item.id}>
-              <td>{item.firstName}</td>
-              <td>{item.lastName}</td>
-              <td>{item.phone}</td>
-              <td>{item.gender}</td>
-              <td>{item.age}</td>
-              <td>
-                <button
-                  className="button"
-                  type="button"
-                  id={item.id}
-                  onClick={deleteContact}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+    const requestSort = sortField => {
+        let direction = 'ascending';
+        if (
+            sortConfig.sortField === sortField &&
+            sortConfig.direction === 'ascending'
+        ) {
+            direction = 'descending';
+        }
+        setSortConfig({ sortField, direction });
+    };
+
+    const getClassNamesFor = name => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.sortField === name ? sortConfig.direction : '';
+    };
+
+    return (
+        <table className="table">
+            <thead className="table-head">
+                <tr className="table-head">
+                    <th className="table-head">
+                        <button
+                            className={
+                                'table-head-btn ' +
+                                getClassNamesFor('firstName')
+                            }
+                            onClick={() => {
+                                requestSort('firstName');
+                            }}
+                        >
+                            firstName
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            className={
+                                'table-head-btn ' + getClassNamesFor('lastName')
+                            }
+                            onClick={() => {
+                                requestSort('lastName');
+                            }}
+                        >
+                            lastName
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            className={
+                                'table-head-btn ' + getClassNamesFor('phone')
+                            }
+                            onClick={() => {
+                                requestSort('phone');
+                            }}
+                        >
+                            phone
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            className={
+                                'table-head-btn ' + getClassNamesFor('gender')
+                            }
+                            onClick={() => {
+                                requestSort('gender');
+                            }}
+                        >
+                            gender
+                        </button>
+                    </th>
+                    <th>
+                        <button
+                            className={
+                                'table-head-btn ' + getClassNamesFor('age')
+                            }
+                            onClick={() => {
+                                requestSort('age');
+                            }}
+                        >
+                            age
+                        </button>
+                    </th>
+                    <th>delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {sortedApplicants.map(item => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.firstName}</td>
+                            <td>{item.lastName}</td>
+                            <td>{item.phone}</td>
+                            <td>{item.gender}</td>
+                            <td>{item.age}</td>
+                            <td>
+                                <button
+                                    className="button"
+                                    type="button"
+                                    id={item.id}
+                                    onClick={deleteContact}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
